ac8dbc41df446304c231dad2714250efd92af3ac
Edoardo Barchiesi <edoardo.barchiesi@hotmail.it>
2025-01-09 16:27:22 +0100

    Update Program.cs
    Secured session cookie

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 ﻿using System.Net;
 using System.Text.RegularExpressions;
 
-const string cookie = "53616c7465645f5fc12e3246b558a254bf575b01114b24d8cfad6e1f437005c5dfea81ee70ea59e56793445c116b17945f23e144ff23399b723b43e4143212b9";
+const string cookie = "[Insert your session cookie here]";
 
 
 await AdventOfCode.Advent2024.DayTwo.DayTwoPartTwo(cookie);
